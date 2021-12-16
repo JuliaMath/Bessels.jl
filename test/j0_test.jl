d5f98200e808c3b69379b3d02d7c7d7bee96b545
Michael Helton <heltonmc@protonmail.com>
2021-12-16 18:52:09 -0500

    add Float 32 j0, y0

--- test/j0_test.jl
+++ test/j0_test.jl
@@ -1,3 +1,9 @@
 @test besselj0(0.0) ≈ 1.0
-x = 0.0:0.001:1000.0
-@test SpecialFunctions.besselj0.(x) ≈ besselj0.(x)
+
+x = 0.001:0.001:1000.0
+j0_SF = SpecialFunctions.besselj0.(x)
+j0_64 = besselj0.(Float64.(x))
+j0_32 = besselj0.(Float32.(x))
+
+@test j0_SF ≈ j0_64
+@test j0_SF ≈ j0_32
--- test/y0_test.jl
+++ test/y0_test.jl
@@ -1,3 +1,7 @@
 x = 0.001:0.001:1000.0
-@test SpecialFunctions.bessely0.(x) ≈ bessely0.(x)
-@test isinf(bessely0(0.0))
+y0_SF = SpecialFunctions.bessely0.(x)
+y0_64 = bessely0.(Float64.(x))
+y0_32 = bessely0.(Float32.(x))
+
+@test y0_SF ≈ y0_64
+@test y0_SF ≈ y0_32
