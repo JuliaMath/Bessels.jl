fe6961d72e47cb4a59d04575bb31faf5ac2e8d5a
Michael Helton <heltonmc@protonmail.com>
2021-12-23 10:07:53 -0600

    update 128 precision j0

--- test/j0_test.jl
+++ test/j0_test.jl
@@ -1,9 +1,20 @@
 @test besselj0(0.0) ≈ 1.0
 
-x = 0.001:0.001:1000.0
-j0_SF = SpecialFunctions.besselj0.(x)
+x = 0.01:0.01:100.0
+
+j0_SpecialFunctions = SpecialFunctions.besselj0.(big.(x))
+@assert j0_SpecialFunctions[1] isa BigFloat
+
 j0_64 = besselj0.(Float64.(x))
 j0_32 = besselj0.(Float32.(x))
+j0_big = besselj0.(big.(x))
 
-@test j0_SF ≈ j0_64
-@test j0_SF ≈ j0_32
+@test j0_64[1] isa Float64
+@test j0_32[1] isa Float32
+@test j0_big[1] isa BigFloat
+
+
+@test j0_64 ≈ j0_SpecialFunctions
+@test j0_32 ≈ j0_SpecialFunctions
+#@show j0_big .- j0_SpecialFunctions
+@test isapprox(j0_big, j0_SpecialFunctions, atol=1.5e-34)
--- test/runtests.jl
+++ test/runtests.jl
@@ -4,5 +4,3 @@ import SpecialFunctions
 
 @time @testset "j0" begin include("j0_test.jl") end
 @time @testset "y0" begin include("y0_test.jl") end
-
-    
