a7c57d736be7666fb0fb1cac476d70e915025c45
Michael Helton <heltonmc@protonmail.com>
2022-01-07 15:58:32 -0600

    update test structure

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,5 @@ using Bessels
 using Test
 import SpecialFunctions
 
-@time @testset "j0" begin include("j0_test.jl") end
-@time @testset "y0" begin include("y0_test.jl") end
+@time @testset "besselj" begin include("besselj_test.jl") end
+@time @testset "bessely" begin include("bessely_test.jl") end
