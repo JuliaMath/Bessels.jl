13cba3f5c010b166b7f7a2cb223d42fe6b8a9972
Michael Helton <heltonmc@protonmail.com>
2022-01-11 09:56:55 -0600

    add hankel, i1, k1

---
+++ src/hankel.jl
@@ -0,0 +1,12 @@
+function besselh(nu::Float64, k::Integer, x::AbstractFloat)
+    if k == 1
+        return complex(besselj(nu, x), bessely(nu, x))
+    elseif k == 2
+        return complex(besselj(nu, x), -bessely(nu, x))
+    else
+        throw(ArgumentError("k must be 1 or 2"))
+    end
+end
+
+hankelh1(nu, z) = besselh(nu, 1, z)
+hankelh2(nu, z) = besselh(nu, 2, z)
