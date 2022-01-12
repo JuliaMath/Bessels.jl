5a4b4b32a2b19fae789a1f262518e86a580603bc
Oscar Smith <oscardssmith@gmail.com>
2022-01-11 21:32:20 -0500

    fix typo

--- src/chebyshev.jl
+++ src/chebyshev.jl
@@ -6,7 +6,7 @@ function chbevl(x::Union{Float32, Float64}, coeff)
     for i in 2:length(coeff)
         b2 = b1
         b1 = b0
-        b0 = muladd(x, b1, b2) + coeff[i]
+        b0 = muladd(x, b1, -b2) + coeff[i]
     end
 
     return (b0 - b2)/2
