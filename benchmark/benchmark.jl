c716b5d0b3227da9418e94d572295791fd9d3630
Michael Helton <heltonmc@protonmail.com>
2022-02-09 14:40:30 -0600

    update benchmarks

--- benchmark/benchmark.jl
+++ benchmark/benchmark.jl
@@ -4,16 +4,16 @@ using Bessels
 suite = BenchmarkGroup()
 
 suite["besseli"] = BenchmarkGroup()
-suite["besseli"]["besseli0"] = @benchmarkable besseli(0, x) setup=(x = rand()*100)
-suite["besseli"]["besseli1"] = @benchmarkable besseli(1, x) setup=(x = rand()*100)
+suite["besseli"]["besseli0"] = @benchmarkable besseli0(x) setup=(x = rand()*100)
+suite["besseli"]["besseli1"] = @benchmarkable besseli1(x) setup=(x = rand()*100)
 suite["besseli"]["besseli20"] = @benchmarkable besseli(20, x) setup=(x = rand()*100)
-suite["besseli"]["besseli200"] = @benchmarkable besseli(120, x) setup=(x = rand()*100)
+suite["besseli"]["besseli120"] = @benchmarkable besseli(120, x) setup=(x = rand()*100 + 1.0)
 
 suite["besselk"] = BenchmarkGroup()
-suite["besselk"]["besselk0"] = @benchmarkable besselk(0, x) setup=(x = rand()*100)
-suite["besselk"]["besselk1"] = @benchmarkable besselk(1, x) setup=(x = rand()*100)
+suite["besselk"]["besselk0"] = @benchmarkable besselk0(x) setup=(x = rand()*100)
+suite["besselk"]["besselk1"] = @benchmarkable besselk1(x) setup=(x = rand()*100)
 suite["besselk"]["besselk20"] = @benchmarkable besselk(20, x) setup=(x = rand()*100)
-suite["besselk"]["besselk200"] = @benchmarkable besselk(120, x) setup=(x = rand()*100)
+suite["besselk"]["besselk120"] = @benchmarkable besselk(120, x) setup=(x = rand()*100 + 1.0)
 
 suite["besselj"] = BenchmarkGroup()
 suite["besselj"]["besselj0"] = @benchmarkable besselj0(x) setup=(x = rand()*100)
