230d7f1bde610cce470b081bc2f82003a658a96e
Michael Helton <heltonmc@protonmail.com>
2022-08-09 13:28:02 -0400

    update tests

--- test/airy_test.jl
+++ test/airy_test.jl
@@ -2,7 +2,7 @@
 # these are prone to some cancellation as they are indirectly calculated from relations to bessel functions
 # this is amplified for negative arguments
 # the implementations provided by SpecialFunctions.jl suffer from same inaccuracies
-for x in [0.0; rand(10000)*100]
+for x in [0.0; 1e-17:0.1:100.0]
     @test isapprox(airyai(x), SpecialFunctions.airyai(x), rtol=1e-12)
     @test isapprox(airyai(-x), SpecialFunctions.airyai(-x), rtol=1e-9)
 
