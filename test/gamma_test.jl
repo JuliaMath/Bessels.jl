a7ef4361aca86a2ca52ac4381ac7ce63a75d915a
Oscar Smith <oscardssmith@gmail.com>
2022-11-04 08:34:00 -0500

    add tests

--- test/gamma_test.jl
+++ test/gamma_test.jl
@@ -1,8 +1,10 @@
-x = rand(10000)*170
-@test SpecialFunctions.gamma.(BigFloat.(x)) ≈ Bessels.gamma.(x)
-@test SpecialFunctions.gamma.(BigFloat.(-x)) ≈ Bessels.gamma.(-x)
-@test isnan(Bessels.gamma(NaN))
-@test isinf(Bessels.gamma(Inf))
+for (T, max) in ((Float16, 13), (Float32, 43), (Float64, 170))
+    x = rand(T, 10000)*max
+    @test SpecialFunctions.gamma.(widen.(x)) ≈ Bessels.gamma.(x)
+    @test SpecialFunctions.gamma.(widen.(-x)) ≈ Bessels.gamma.(-x)
+    @test isnan(Bessels.gamma(T(NaN)))
+    @test isinf(Bessels.gamma(T(Inf)))
+end
 
 x = [0, 1, 2, 3, 8, 15, 20, 30]
 @test SpecialFunctions.gamma.(x) ≈ Bessels.gamma.(x)
