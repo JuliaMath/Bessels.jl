1b930648f60f29845334fe32f36619ff6a3f9269
Oscar Smith <oscardssmith@gmail.com>
2022-11-05 15:03:56 -0500

    improve tests

--- test/gamma_test.jl
+++ test/gamma_test.jl
@@ -1,11 +1,11 @@
 for (T, max) in ((Float16, 13), (Float32, 43), (Float64, 170))
     v = rand(T, 10000)*max
     for x in v
-        @test T(SpecialFunctions.gamma(widen(x))) ≈ Bessels.gamma(x)
+        @test isapprox(T(SpecialFunctions.gamma(widen(x))), Bessels.gamma(x)
         if isinteger(x)
             @test_throws DomainError Bessels.gamma(-x)
         else
-            @test isapprox(T(SpecialFunctions.gamma(widen(-x))), Bessels.gamma(-x), atol=nextfloat(Float16(0.),2))
+            @test isapprox(T(SpecialFunctions.gamma(widen(-x))), Bessels.gamma(-x), atol=nextfloat(T(0.),2), rtol=1.5*eps(T))
         end
     end
     @test isnan(Bessels.gamma(T(NaN)))
