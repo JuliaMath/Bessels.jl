fb17d4e0a11784145460e3dc46251b4d3a3885f2
Oscar Smith <oscardssmith@gmail.com>
2022-11-05 13:36:52 -0500

    fix tests

--- test/gamma_test.jl
+++ test/gamma_test.jl
@@ -1,7 +1,11 @@
 for (T, max) in ((Float16, 13), (Float32, 43), (Float64, 170))
     x = rand(T, 10000)*max
     @test T.(SpecialFunctions.gamma.(widen.(x))) ≈ Bessels.gamma.(x)
-    @test T.(SpecialFunctions.gamma.(widen.(-x))) ≈ Bessels.gamma.(-x)
+    if isinteger(x)
+        @test_throws DomainError Bessels.gamma.(-x)
+    else
+        @test T.(SpecialFunctions.gamma.(widen.(-x))) ≈ Bessels.gamma.(-x)
+    end
     @test isnan(Bessels.gamma(T(NaN)))
     @test isinf(Bessels.gamma(T(Inf)))
 end
