ffbc84d3719b5a2caf2aab64d8464842f2e020c9
Oscar Smith <oscardssmith@gmail.com>
2022-11-06 23:12:57 -0600

    update test tolerances

--- test/gamma_test.jl
+++ test/gamma_test.jl
@@ -1,11 +1,11 @@
-for (T, max) in ((Float16, 13), (Float32, 43), (Float64, 170))
+for (T, max, rtol) in ((Float16, 13, 1.0), (Float32, 43, 1.0), (Float64, 170, 7))
     v = rand(T, 10000)*max
     for x in v
-        @test isapprox(T(SpecialFunctions.gamma(widen(x))), Bessels.gamma(x), rtol=1.5*eps(T))
+        @test isapprox(T(SpecialFunctions.gamma(widen(x))), Bessels.gamma(x), rtol=rtol*eps(T))
         if isinteger(x)
             @test_throws DomainError Bessels.gamma(-x)
         else
-            @test isapprox(T(SpecialFunctions.gamma(widen(-x))), Bessels.gamma(-x), atol=nextfloat(T(0.),2), rtol=1.5*eps(T))
+            @test isapprox(T(SpecialFunctions.gamma(widen(-x))), Bessels.gamma(-x), atol=nextfloat(T(0.),2), rtol=rtol*eps(T))
         end
     end
     @test isnan(Bessels.gamma(T(NaN)))
