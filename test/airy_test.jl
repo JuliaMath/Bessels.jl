3a3e094af347f45a1b9ca218be9cb6674c38ed6c
Michael Helton <heltonmc@protonmail.com>
2023-02-09 15:37:09 -0500

    update functions/tests

--- test/airy_test.jl
+++ test/airy_test.jl
@@ -1,7 +1,67 @@
 # test the airy functions
 # these are prone to some cancellation as they are indirectly calculated from relations to bessel functions
 # this is amplified for negative arguments
-# the implementations provided by SpecialFunctions.jl suffer from same inaccuracies
+
+function arb_airyaix(x)
+    x = ArbNumerics.ArbFloat(x)
+    return Float64(ArbNumerics.airyai(x) * exp(2 * x * sqrt(x) / 3))
+end
+function arb_airyaiprimex(x)
+    x = ArbNumerics.ArbFloat(x)
+    return Float64(ArbNumerics.airyaiprime(x) * exp(2 * x * sqrt(x) / 3))
+end
+function arb_airyai(x)
+    x = ArbNumerics.ArbFloat(x)
+    return Float64(ArbNumerics.airyai(x))
+end
+function arb_airyaiprime(x)
+    x = ArbNumerics.ArbFloat(x)
+    return Float64(ArbNumerics.airyaiprime(x))
+end
+function arb_airybi(x)
+    x = ArbNumerics.ArbFloat(x)
+    return Float64(ArbNumerics.airybi(x))
+end
+function arb_airybiprime(x)
+    x = ArbNumerics.ArbFloat(x)
+    return Float64(ArbNumerics.airybiprime(x))
+end
+function arb_airybix(x)
+    x = ArbNumerics.ArbFloat(x)
+    return Float64(ArbNumerics.airybi(x) * exp(-2 * x * sqrt(complex(x)) / 3))
+end
+function arb_airybiprimex(x)
+    x = ArbNumerics.ArbFloat(x)
+    return Float64(ArbNumerics.airybiprime(x) * exp(-2 * x * sqrt(complex(x)) / 3))
+end
+
+x = rand(10000)*105.0
+for _x in x
+    @show _x
+    @test isapprox(airyai(_x), arb_airyai(_x), rtol=5e-13)
+    @test isapprox(airyaiprime(_x), arb_airyaiprime(_x), rtol=6e-13)
+    @test isapprox(airyai(-_x), arb_airyai(-_x), atol=5e-14)
+    @test isapprox(airyaiprime(-_x), arb_airyaiprime(-_x), rtol=5e-11, atol=1e-13)
+
+
+    @test isapprox(Bessels.airyaix(_x), arb_airyaix(_x), rtol=8e-16)
+    @test isapprox(Bessels.airyaiprimex(_x), arb_airyaiprimex(_x), rtol=8e-16)
+
+
+    @test isapprox(airybi(_x), arb_airybi(_x), rtol=5e-13)
+    @test isapprox(airybiprime(_x), arb_airybiprime(_x), rtol=5e-13)
+
+    @test isapprox(airybi(-_x), arb_airybi(-_x), atol=4e-14)
+    @test isapprox(airybiprime(-_x), arb_airybiprime(-_x), rtol=8e-11, atol=4e-14)
+
+    @test isapprox(Bessels.airybix(_x), arb_airybix(_x), rtol=4e-15)
+    @test isapprox(Bessels.airybiprimex(_x), arb_airybiprimex(_x), rtol=4e-15, atol=5e-14)
+
+    @test isapprox(Bessels.airybix(-_x), arb_airybi(-_x), atol=8e-14) # scaled should be same for neg args
+
+end
+
+#=
 for x in [0.0; 1e-17:0.1:100.0]
     @test isapprox(airyai(x), SpecialFunctions.airyai(x), rtol=2e-13)
     @test isapprox(airyai(-x), SpecialFunctions.airyai(-x), rtol=3e-12)
@@ -65,3 +125,4 @@ end
 @test airybi(ComplexF16(1.2)) isa ComplexF16
 @test airybiprime(Float16(1.9)) isa Float16
 @test airybiprime(ComplexF16(1.2)) isa ComplexF16
+=#
