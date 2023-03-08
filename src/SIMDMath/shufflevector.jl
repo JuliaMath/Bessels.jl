38aae7c6d8e2ef302ce7012d16b105ef0a2c276b
Michael Helton <heltonmc@protonmail.com>
2023-03-08 15:31:17 -0500

    add generic constant vector [skip ci]

--- src/SIMDMath/shufflevector.jl
+++ src/SIMDMath/shufflevector.jl
@@ -1,28 +1,14 @@
 # create tuples of VecElements filled with a constant value of x
 
-@inline constantvector(x::Float64, y) = constantvector(VE(x), y)
+@inline constantvector(x::T, y) where T <: FloatTypes = constantvector(VE(x), y)
 
-@inline function constantvector(x::VecElement{Float64}, y::Type{LVec{2, Float64}})
-    llvmcall("""%2 = insertelement <2 x double> undef, double %0, i32 0
-                %3 = shufflevector <2 x double> %2, <2 x double> undef, <2 x i32> zeroinitializer
-                ret <2 x double> %3""",
-        LVec{2, Float64},
-        Tuple{VecElement{Float64}},
-        x)
-end
-@inline function constantvector(x::VecElement{Float64}, y::Type{LVec{4, Float64}})
-    llvmcall("""%2 = insertelement <4 x double> undef, double %0, i32 0
-                %3 = shufflevector <4 x double> %2, <4 x double> undef, <4 x i32> zeroinitializer
-                ret <4 x double> %3""",
-        LVec{4, Float64},
-        Tuple{VecElement{Float64}},
-        x)
-end
-@inline function constantvector(x::VecElement{Float64}, y::Type{LVec{8, Float64}})
-    llvmcall("""%2 = insertelement <8 x double> undef, double %0, i32 0
-                %3 = shufflevector <8 x double> %2, <8 x double> undef, <8 x i32> zeroinitializer
-                ret <8 x double> %3""",
-        LVec{8, Float64},
-        Tuple{VecElement{Float64}},
-        x)
+@inline @generated function constantvector(x::VecElement{T}, y::Type{LVec{N, T}}) where {N, T <: FloatTypes}
+    s = """
+        %2 = insertelement <$N x $(LLVMType[T])> undef, $(LLVMType[T]) %0, i32 0
+        %3 = shufflevector <$N x $(LLVMType[T])> %2, <$N x $(LLVMType[T])> undef, <$N x i32> zeroinitializer
+        ret <$N x $(LLVMType[T])> %3
+        """
+    return :(
+        llvmcall($s, LVec{N, T}, Tuple{VecElement{T}}, x)
+        )
 end
