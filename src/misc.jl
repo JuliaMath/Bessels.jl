df002cc927953613b233321bf4a29c3d0e3920ef
oscarddssmith <oscar.smith@juliacomputing.com>
2023-04-07 15:50:59 -0400

    better sin_sum

--- src/misc.jl
+++ src/misc.jl
@@ -1,47 +1,69 @@
-# function to more accurately compute cos(x + xn)
-# see https://github.com/heltonmc/Bessels.jl/pull/13
-# written by @oscardssmith
-function cos_sum(x, xn)
-    s = x + xn
-    n, r = reduce_pi02_med(s)
-    lo = r.lo - ((s - x) - xn)
-    hi = r.hi + lo
-    y = Base.Math.DoubleFloat64(hi, r.hi-hi+lo)
+using Base.Math: sin_kernel, cos_kernel, rem_pio2_kernel, DoubleFloat64, DoubleFloat32
+function sin_double(n,y)
     n = n&3
     if n == 0
-        return Base.Math.cos_kernel(y)
+        return sin_kernel(y)
     elseif n == 1
-        return -Base.Math.sin_kernel(y)
+        return cos_kernel(y)
     elseif n == 2
-        return -Base.Math.cos_kernel(y)
+        return -sin_kernel(y)
     else
-        return Base.Math.sin_kernel(y)
+        return -cos_kernel(y)
     end
 end
-# function to more accurately compute sin(x + xn)
-function sin_sum(x, xn)
-    s = x + xn
-    n, r = reduce_pi02_med(s)
-    lo = r.lo - ((s - x) - xn)
-    hi = r.hi + lo
-    y = Base.Math.DoubleFloat64(hi, r.hi-hi+lo)
-    n = n&3
-    if n == 0
-        return Base.Math.sin_kernel(y)
-    elseif n == 1
-        return Base.Math.cos_kernel(y)
-    elseif n == 2
-        return -Base.Math.sin_kernel(y)
-    else
-        return -Base.Math.cos_kernel(y)
-    end
-end
-@inline function reduce_pi02_med(x::Float64)
-    pio2_1 = 1.57079632673412561417e+00
 
-    fn = round(x*(2/pi))
-    r  = muladd(-fn, pio2_1, x)
-    w  = fn * 6.07710050650619224932e-11
-    y = r-w
-    return unsafe_trunc(Int, fn), Base.Math.DoubleFloat64(y, (r-y)-w)
+"""
+    computes sin(sum(xs)+sum(xlos)) where xs, xlos are sorted by absolute value
+    and xlos are all <=pi/4 (and therefore don't need to be reduced)
+    Doing this is much more accurate than the naive sin(sum(xs)+sum(xlos))
+"""
+function sin_sum(xs::Tuple{Vararg{Float64}}, xlos::Tuple{Vararg{Float64}})
+    n = 0
+    hi, lo = 0.0, 0.0
+    for x in xs
+        n_i, y = rem_pio2_kernel(x)
+        n += n_i
+        s = y.hi + hi
+        lo += (y.hi - (s - hi)) + y.lo
+        hi = s
+    end
+    for x in xlos
+        s = x + hi
+        lo += (x - (s - hi))
+        hi = s
+    end
+    while hi > pi/4
+        hi -= pi/2
+        lo -= 6.123233995736766e-17
+        n += 1
+    end
+    while hi < -pi/4
+        hi += pi/2
+        lo += 6.123233995736766e-17
+        n -= 1
+    end
+    sin_double(n,DoubleFloat64(hi, lo))
+end
+
+function sin_sum(xs::Tuple{Vararg{Float32}}, xlos::Tuple{Vararg{Float32}})
+    n = 0
+    y = 0.0
+    for x in xs
+        n_i, y_i = rem_pio2_kernel(x)
+        n += n_i
+        y += y_i.hi
+    end
+    for x in xlos
+        y += x
+    end
+    while y > pi/4
+        y -= pi/2
+        n += 1
+    end
+    while y < -pi/4
+        y += pi/2
+        n -= 1
+    end
+
+    sin_double(n,DoubleFloat32(y))
 end
