fcec1e523ebe85f4072eb5e5b241417d7c1d8a0f
Oscar Smith <oscardssmith@gmail.com>
2025-04-23 23:08:06 -0400

    add tests

--- test/hankel_test.jl
+++ test/hankel_test.jl
@@ -3,31 +3,39 @@
 # here we will test just a few cases of the overall hankel function
 # focusing on negative arguments and reflection
 
-v, x = 1.5, 1.3
-@test isapprox(hankelh1(v, x), SpecialFunctions.hankelh1(v, x), rtol=2e-13)
-@test isapprox(hankelh2(v, x), SpecialFunctions.hankelh2(v, x), rtol=2e-13)
-@test isapprox(besselh(v, 1, x), SpecialFunctions.besselh(v, 1, x), rtol=2e-13)
-@test isapprox(besselh(v, 2, x), SpecialFunctions.besselh(v, 2, x), rtol=2e-13)
+@testset "$T" for T in (Float32, Float64)
+    rtol = T == Float64 ? 2e-13 : 2e-6
+    v, x = T(1.5), T(1.3)
+    @test isapprox(hankelh1(v, x), SpecialFunctions.hankelh1(v, x); rtol)
+    @test isapprox(hankelh2(v, x), SpecialFunctions.hankelh2(v, x); rtol)
+    @test isapprox(besselh(v, 1, x), SpecialFunctions.besselh(v, 1, x); rtol)
+    @test isapprox(besselh(v, 2, x), SpecialFunctions.besselh(v, 2, x); rtol)
+    @inferred besselh(v, 2, x)
+    
+    v, x = T(-2.6), T(9.2)
+    @test isapprox(hankelh1(v, x), SpecialFunctions.hankelh1(v, x); rtol)
+    @test isapprox(hankelh2(v, x), SpecialFunctions.hankelh2(v, x); rtol)
+    @test isapprox(besselh(v, 1, x), SpecialFunctions.besselh(v, 1, x); rtol)
+    @test isapprox(besselh(v, 2, x), SpecialFunctions.besselh(v, 2, x); rtol)
+    @inferred besselh(v, 2, x)
 
-v, x = -2.6, 9.2
-@test isapprox(hankelh1(v, x), SpecialFunctions.hankelh1(v, x), rtol=2e-13)
-@test isapprox(hankelh2(v, x), SpecialFunctions.hankelh2(v, x), rtol=2e-13)
-@test isapprox(besselh(v, 1, x), SpecialFunctions.besselh(v, 1, x), rtol=2e-13)
-@test isapprox(besselh(v, 2, x), SpecialFunctions.besselh(v, 2, x), rtol=2e-13)
+    v, x = T(-4.0), T(11.4)
+    @test isapprox(hankelh1(v, x), SpecialFunctions.hankelh1(v, x); rtol)
+    @test isapprox(hankelh2(v, x), SpecialFunctions.hankelh2(v, x); rtol)
+    @test isapprox(besselh(v, 1, x), SpecialFunctions.besselh(v, 1, x); rtol)
+    @test isapprox(besselh(v, 2, x), SpecialFunctions.besselh(v, 2, x); rtol)
+    @inferred besselh(v, 2, x)
 
-v, x = -4.0, 11.4
-@test isapprox(hankelh1(v, x), SpecialFunctions.hankelh1(v, x), rtol=2e-13)
-@test isapprox(hankelh2(v, x), SpecialFunctions.hankelh2(v, x), rtol=2e-13)
-@test isapprox(besselh(v, 1, x), SpecialFunctions.besselh(v, 1, x), rtol=2e-13)
-@test isapprox(besselh(v, 2, x), SpecialFunctions.besselh(v, 2, x), rtol=2e-13)
+    v, x = T(14.3), T(29.4)
+    @test isapprox(hankelh1(v, x), SpecialFunctions.hankelh1(v, x); rtol)
+    @test isapprox(hankelh2(v, x), SpecialFunctions.hankelh2(v, x); rtol)
+    @test isapprox(besselh(v, 1, x), SpecialFunctions.besselh(v, 1, x); rtol)
+    @test isapprox(besselh(v, 2, x), SpecialFunctions.besselh(v, 2, x); rtol)
+    @inferred besselh(v, 2, x)
 
-v, x = 14.3, 29.4
-@test isapprox(hankelh1(v, x), SpecialFunctions.hankelh1(v, x), rtol=2e-13)
-@test isapprox(hankelh2(v, x), SpecialFunctions.hankelh2(v, x), rtol=2e-13)
-@test isapprox(besselh(v, 1, x), SpecialFunctions.besselh(v, 1, x), rtol=2e-13)
-@test isapprox(besselh(v, 2, x), SpecialFunctions.besselh(v, 2, x), rtol=2e-13)
-
-@test isapprox(hankelh1(1:50, 10.0), SpecialFunctions.hankelh1.(1:50, 10.0), rtol=2e-13)
-@test isapprox(hankelh1(0.5:1:25.5, 15.0), SpecialFunctions.hankelh1.(0.5:1:25.5, 15.0), rtol=2e-13)
-@test isapprox(hankelh1(1:50, 100.0), SpecialFunctions.hankelh1.(1:50, 100.0), rtol=2e-13)
-@test isapprox(hankelh2(1:50, 10.0), SpecialFunctions.hankelh2.(1:50, 10.0), rtol=2e-13)
+    @test isapprox(hankelh1(1:50, T(10)), SpecialFunctions.hankelh1.(1:50, 10.0); rtol)
+    @test isapprox(hankelh1(T(0.5):T(25.5), T(15)), SpecialFunctions.hankelh1.(0.5:1:25.5, 15.0); rtol)
+    @test isapprox(hankelh1(1:50, T(100)), SpecialFunctions.hankelh1.(1:50, 100.0); 2*rtol)
+    @test isapprox(hankelh2(1:50, T(10)), SpecialFunctions.hankelh2.(1:50, 10.0); rtol)
+    @inferred hankelh2(1:50, T(10))
+end
